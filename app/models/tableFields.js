daf0176f3659aef244e311aeee6c051113f84453
xiaobxia <673806687@qq.com>
2018-12-13 09:43:54 +0800

    xg

--- app/models/tableFields.js
+++ app/models/tableFields.js
@@ -1,14 +1,17 @@
+/**
+ * resBase 非敏感数据，面向用户
+ */
 module.exports = {
   user: {
     resBase: ['_id', 'name', 'token']
   },
   schedule: {
-    resBase: ['_id', 'name', 'describe', 'type', 'open', 'create_at']
+    resBase: ['_id', 'name', 'describe', 'type', 'open']
   },
   logAudit: {
-    resBase: ['_id', 'log_type', 'user_id', 'user_name', 'platform', 'create_at']
+    resBase: ['_id', 'log_type', 'user_id', 'user_name', 'platform']
   },
   dictionary: {
-    resBase: ['_id', 'key', 'user_id', 'user_name', 'platform', 'create_at']
+    resBase: ['_id', 'key', 'describe', 'type', 'value']
   }
 }

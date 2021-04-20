71f69b7c63e34baf6bc4099effede02daa758120
xiaobxia <673806687@qq.com>
2021-04-20 10:01:30 +0800

    xg

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   db: 'mongodb://serviceBase:serviceBase@47.98.140.76:27017/serviceBase',
   // 邮件配置
   email: {
+    // 发送邮件账户
     senderAccount: {
       host: 'smtp.mxhichina.com',
       secureConnection: false, // use SSL
@@ -9,7 +10,9 @@ module.exports = {
       port: 25,
       // secure: true, // use TLS
       auth: {
+        // 账户
         user: '',
+        // 密码
         pass: ''
       },
       ignoreTLS: true
@@ -17,6 +20,7 @@ module.exports = {
     adminAccount: {
       user: ''
     },
+    // 显示的来源（用户看到是谁发来的）
     formName: 'Xiaobxia'
   }
 }
--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,7 @@ module.exports = {
   db: 'mongodb://serviceBase:serviceBase@localhost:27017/serviceBase',
   // 邮件配置
   email: {
+    // 发送邮件账户
     senderAccount: {
       host: 'smtp.mxhichina.com',
       secureConnection: true, // use SSL
@@ -17,6 +18,7 @@ module.exports = {
     adminAccount: {
       user: ''
     },
+    // 显示的来源（用户看到是谁发来的）
     formName: 'Xiaobxia'
   }
 }
